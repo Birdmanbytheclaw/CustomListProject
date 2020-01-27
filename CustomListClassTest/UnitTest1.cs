3b233bde8a9007b77abef8f836c636f7900085df
Birdmanbytheclaw <therealparkerflynn@gmail.com>
2020-01-27 10:26:25 -0600

    CheckingCapacity written just need to validate then i will be starting ny method creation

--- CustomListClassTest/UnitTest1.cs
+++ CustomListClassTest/UnitTest1.cs
@@ -36,6 +36,8 @@ namespace CustomListClassTest
             //assert
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
         public void ListIndexNumberCount()
         {
             //Test Count
@@ -53,44 +55,83 @@ namespace CustomListClassTest
             Assert.AreEqual(expected, actual);
         }
         //Make sure 0 does not get overwritten
+        [TestMethod]
         public void CheckingZeroIndewForOverwtite()
         {
-        //Arrange
-        CustomList<int> ListOfMine = new CustomList<int>();
-        int actual;
-        int Value = 2;
-        int SecondValue = 17;
+            //Arrange
+            CustomList<int> ListOfMine = new CustomList<int>();
+            int actual;
+            int Value = 2;
+            int SecondValue = 17;
             //Act
             ListOfMine.Add(Value);
             ListOfMine.Add(SecondValue);
             actual = ListOfMine[1];
 
             //Assert
-        Assert.AreEqual(expected, actual);
-      
+            Assert.AreEqual(expected, actual);
+
         }
-    //test adding the same thing twice and both exist
-    public void AddingSameThingTwice()
-    {
-    //Arrange
+        //test adding the same thing twice and both exist
+        [TestMethod]
+        public void AddingSameThingTwice()
+        {
+            //Arrange
+            CustomList<string> ListOfMine = new CustomList<string>();
+            string ValueZero = "Hello";
+            String Actual;
 
-    //Act
+            //Act
+            ListOfMine.Add(ValueZero);
+            ListOfMine.Add(ValueZero);
+            ListOfMine.Add(ValueZero);
+            ListOfMine.Add(ValueZero);
+            ListOfMine.Add(ValueZero);
+            ListOfMine.Add(ValueZero);
+            ListOfMine.Add(ValueZero);
+            Actual = ListOfMine[6]
 
-    //Assert
-    Assert.AreEqual(expected, actual);
-      }
+            //Assert
+            Assert.AreEqual(ValueZero, Actual);
+        }
         //test capacity Growth after 5 are added
+        [TestMethod]
+        public void GrowCapacityToCheckForStoredValue();
        { 
         //Arrange
-
+        CustomList<string> ListOfMine = new CustomList<string>();
+        string elephant = "elephants";
+        string are = "are";
+        string dancing = "dancing";
+        string get = "get";
+        string back = "back";
+        int actual;
+        int expected = 8;
         //Act
-
+        ListOfMine.Add(elephant);
+        ListOfMine.Add(are);
+        ListOfMine.Add(dancing);
+        ListOfMine.Add(get);
+        ListOfMine.Add(back);
+         actually = ListOfMine.Capacity;
         //Assert
         Assert.AreEqual(expected, actual);
        }
-      
+    //Capacity at Four
+    [TestMethod]
+    public void AddAdditionalValueCapacityFour();
+            {
+    //arange
+    CustomList<String> ListOfMine = new CustomList<string>();
+    string ValueZero = "something";
+    interface actual;
+    interface expected = 4;
+    //act
+    ListofMine.Add(ValueZero);
+    actual = ListOfMine.Capacity;
+    //assert
+    Assert.AreEqual(expected, actual);
+            }
         
-
-        
-    }
+   }
 }
